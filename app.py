9deff7b4f71291795c28e83a672b4b41f34e3835
Sar/Ansh <40174034+IOxCyber@users.noreply.github.com>
2025-11-02 19:05:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Sweet Bud 🦭! CI/CD is on."
+    return "Hello, Sweet Bud 🦭! CI/CD is on now."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
